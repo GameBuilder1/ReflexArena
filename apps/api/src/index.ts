145e0c85db21874e54f7ed8d6c8eadb3432608fd
Compliledger <150187165+Mharris40@users.noreply.github.com>
2026-03-11 19:00:51 -0400

    Import scoresRouter in index.ts

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import scoresRouter from "./routes/scores";
 
 import authRouter from "./routes/auth";
 import profileRouter from "./routes/profile";
